3a255a7dea0a43d0f49bf3434a569b3eb960c1ec
Gabriel Cedraz <cedrazdev@gmail.com>
2025-01-13 15:53:45 -0300

    implementa o hook use query para fazer uma chamada a api

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,30 @@
+// O hook use query do react-query é utilizado para fazer requisições a uma API
+import {useQuery, QueryClient} from '@tanstack/react-query'
+
+const queryClient = new QueryClient()
+
+interface IUser {
+    id: number;
+    name: string;
+    email: string;
+}
+
 export function App () {
+    const {data} = useQuery({
+        queryKey: ['users'],
+        queryFn: async (): Promise<IUser[]> => {
+            const response = await fetch("http://localhost:3333/users")
+            return response.json()
+        }
+    }, queryClient);
   return (
-    <h1>Hello World</h1>
+    <div>
+        {data?.map(user => (
+            <div key={user.id}>
+                <strong className='block'>{user.name}</strong>
+                <small>{user.email}</small>
+            </div>
+        ))}
+    </div>
   )
 }
