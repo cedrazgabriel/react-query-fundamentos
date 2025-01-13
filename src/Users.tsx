880016e6554e30097da5a6c2c81d58ff179acf1f
Gabriel Cedraz <cedrazdev@gmail.com>
2025-01-13 18:09:14 -0300

    entendimento sobre os estados de consulta do react query

--- src/Users.tsx
+++ src/Users.tsx
@@ -1,17 +1,36 @@
 import { useQuery } from "@tanstack/react-query";
 import { IUser } from "./types";
+import { sleep } from "./sleep";
 
 export function Users() {
-    const {data} = useQuery({
+
+    const {data, isLoading, isFetching, refetch} = useQuery({
+        enabled: false,
         queryKey: ['users'],
         queryFn: async (): Promise<IUser[]> => {
+            await sleep()
             const response = await fetch("http://localhost:3333/users")
             return response.json()
         }
     });
 
+    /* sobre os estados de consulta do react query
+    isPending(bool): true quando não houver nenhum valor no cache
+    isFetching(bool): true SEMPRE que a queryFn estiver executando (seja na primeira execução, ou nas subsequentes)
+    isLoading(bool): true quando isFetching for true e isPending for true (quando não tem dados no cache e a queryFn está executando)
+    */
+
     return (
-        <div>
+        <div className="p-4">
+        <button
+        className="bg-white text-black px-4 py-2 rounded-lg  "
+         type="button"
+         onClick={() => refetch()}
+         >
+            Listar usuários
+        </button>
+        {isLoading && <h1>Carregando..</h1>}
+        {!isLoading && isFetching && <small>Fetching..</small>}
         {data?.map(user => (
             <div key={user.id}>
                 <strong className='block'>{user.name}</strong>
