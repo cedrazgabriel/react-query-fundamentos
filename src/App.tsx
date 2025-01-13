e471e07d1bad6713a5b407cdc110259c4a73bcf6
Gabriel Cedraz <cedrazdev@gmail.com>
2025-01-13 16:08:38 -0300

    implementa o query client provider passando o query client

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,14 @@
 // O hook use query do react-query é utilizado para fazer requisições a uma API
-import {useQuery, QueryClient} from '@tanstack/react-query'
+import {QueryClient, QueryClientProvider} from '@tanstack/react-query'
+import { Users } from './Users'
 
 const queryClient = new QueryClient()
 
-interface IUser {
-    id: number;
-    name: string;
-    email: string;
-}
-
 export function App () {
-    const {data} = useQuery({
-        queryKey: ['users'],
-        queryFn: async (): Promise<IUser[]> => {
-            const response = await fetch("http://localhost:3333/users")
-            return response.json()
-        }
-    }, queryClient);
+   
   return (
-    <div>
-        {data?.map(user => (
-            <div key={user.id}>
-                <strong className='block'>{user.name}</strong>
-                <small>{user.email}</small>
-            </div>
-        ))}
-    </div>
+   <QueryClientProvider client={queryClient}>
+    <Users />
+    </QueryClientProvider>
   )
 }
---
+++ src/Users.tsx
@@ -0,0 +1,23 @@
+import { useQuery } from "@tanstack/react-query";
+import { IUser } from "./types";
+
+export function Users() {
+    const {data} = useQuery({
+        queryKey: ['users'],
+        queryFn: async (): Promise<IUser[]> => {
+            const response = await fetch("http://localhost:3333/users")
+            return response.json()
+        }
+    });
+
+    return (
+        <div>
+        {data?.map(user => (
+            <div key={user.id}>
+                <strong className='block'>{user.name}</strong>
+                <small>{user.email}</small>
+            </div>
+        ))}
+    </div>
+    )
+}
