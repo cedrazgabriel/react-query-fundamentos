d87e9cd99955ad01e072f0abd21cb6615c40c755
Gabriel Cedraz <cedrazdev@gmail.com>
2025-01-14 09:59:44 -0300

    implementa react query dev tools

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 // O hook use query do react-query é utilizado para fazer requisições a uma API
 import {QueryClient, QueryClientProvider} from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { Users } from './Users'
 
 const queryClient = new QueryClient()
@@ -9,6 +10,7 @@ export function App () {
   return (
    <QueryClientProvider client={queryClient}>
     <Users />
+    <ReactQueryDevtools />
     </QueryClientProvider>
   )
 }
