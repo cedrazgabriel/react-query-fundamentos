5b99db530285396de95a7140971844a9943995ac
Gabriel Cedraz <cedrazdev@gmail.com>
2025-01-17 17:24:27 -0300

    utiliza a primeira mutation

--- src/Users.tsx
+++ src/Users.tsx
@@ -1,4 +1,6 @@
+import React, { useState } from "react";
 import { useUsers } from "./hooks/useUsers";
+import { useMutation } from "@tanstack/react-query";
 
 export function Users() {
 
@@ -10,8 +12,46 @@ export function Users() {
     isLoading(bool): true quando isFetching for true e isPending for true (quando não tem dados no cache e a queryFn está executando)
     */
 
+    const [name, setName] = useState('')
+    const [email, setEmail] = useState('')
+
+    const { mutate } = useMutation({
+        mutationFn: async () => {
+            console.log('mutation fn executada')
+        }
+    })
+
+    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault()
+
+        console.log({ name, email })
+        mutate()
+    }
+
     return (
         <div className="p-4">
+            <div className="mb-3">
+                <form className="flex flex-col gap-2" onSubmit={handleSubmit}>
+                    <input
+                        className="outline-none p1 rounded-md text-zinc-950"
+                        type="text"
+                        placeholder="Nome"
+                        value={name}
+                        onChange={e => setName(e.target.value)}
+                    />
+                    <input
+                        className="outline-none p1 rounded-md text-zinc-950"
+                        type="text"
+                        placeholder="Email"
+                        value={email}
+                        onChange={e => setEmail(e.target.value)}
+                    />
+
+                    <button className="bg-blue-400 py-2 text-zinc-950 rounded-md">
+                        Cadastrar
+                    </button>
+                </form>
+            </div>
             <button
                 className="bg-white text-black px-4 py-2 rounded-lg  "
                 type="button"
